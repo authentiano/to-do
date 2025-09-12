11eb6c73dfad9acf0ae8c2ce3866a178d7878d3e
authentiano <emmasanauthentiano@gmail.com>
2025-09-12 17:48:46 +0000

    models migrations

--- to-do/backend/todo/models.py
+++ to-do/backend/todo/models.py
@@ -6,4 +6,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default =False)
-    
+
+    def __str__(self):
+        return self.title
